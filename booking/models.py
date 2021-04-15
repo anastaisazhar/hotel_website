dd566aafd5ad7372fe42b29da49342c7f8b7f89a
Akbermet Zhuma <akbermet.zh@gmail.com>
2021-04-15 20:21:04 +0200

    added and registered models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    small_size = 's'
+    medium_size = 'm'
+    large_size = 'l'
+
+    room_types = [
+        (small_size, 'small'),
+        (medium_size, 'medium'),
+        (large_size, 'large'),
+    ]
+
+    number = models.CharField(default='',max_length=100)
+    room_type = models.CharField(max_length=1,choices=room_types,default=small_size) 
+    view = models.BooleanField()
+    price = models.IntegerField(default=0)
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.CASCADE,related_name='all_bookings')
+    first_name = models.CharField(default='',max_length=100)
+    second_name = models.CharField(default='',max_length=100)
+    email = models.CharField(default='',max_length=100)
+    checkin_date = models.DateTimeField('check-in date')
+    checkout_date = models.DateTimeField('check-out date')
--- hawaiian_hotel/settings.py
+++ hawaiian_hotel/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+    'booking',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

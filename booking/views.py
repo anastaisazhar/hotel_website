e88d20c3395680aa7664ab8ea72cf04c31caa619
Akbermet Zhuma <akbermet.zh@gmail.com>
2021-04-21 21:38:52 +0200

    booking/list_all_rooms view is working

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 from .models import Room
 
 # Create your views here.
 
 def list_of_rooms(request):
-    room_types_list = Room.objects.all()
-    return render(request, 'booking/list_of_rooms.html')
+    rooms_all = Room.objects.all()
+    template = loader.get_template('booking/list_of_rooms.html')
+    context = {
+        'rooms_all': rooms_all,
+    }
+    return  HttpResponse(template.render(context, request))
